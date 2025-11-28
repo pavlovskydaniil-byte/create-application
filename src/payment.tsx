158ece8d414ebb91d72c38c5cf543162d5ff7550
pavlovskydaniil-byte <pavlovskydaniil@gmail.com>
2025-11-28 21:31:01 +0300

    add final logic

---
+++ src/payment.tsx
@@ -0,0 +1,9 @@
+export default function payment() {
+  return (
+    <div className="w-full h-full mt-[36px] flex flex-col items-center justify-center">
+      <div className="w-[457px] h-[464px] pl-[20px] pr-[20px] border border-[#d9dee2] rounded-[10px] flex flex-col items-center justify-center">
+        <h1 className="mt-[20px] text-title">Обработка платежа...</h1>
+      </div>
+    </div>
+  );
+}
